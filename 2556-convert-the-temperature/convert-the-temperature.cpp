689cfac4d10f6459af2effb3b06b4ab2897727e5
Ravi srivastav <83718332+Ravi-av@users.noreply.github.com>
2024-08-18 21:58:22 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (22.58%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+
+
+double kelvin = celsius+273.15;
+double Fahrenheit = celsius*1.80+32.00;
+vector<double>ans;
+ans.push_back(kelvin);
+ans.push_back(Fahrenheit);
+return ans;
+
+return ans;
+    }
+};
